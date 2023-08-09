4902b1722e4d9f34d7189ff60f9f1856b743483e
Divya Dattatray Gadekar <116143709+divya-gadekar28@users.noreply.github.com>
2023-08-09 20:07:11 +0530

    Add files via upload

---
+++ PracticeQuestions/Question3.cpp
@@ -0,0 +1,25 @@
+//reverse integer
+
+#include<iostream>
+#include<limits.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n;
+    int digit=0,ans;
+    cout<<"enter any number : ";
+    cin>>n;
+
+    while(n!=0){
+        ans=n%10;     //gets last digit
+    //for number whose range will go out of integer range then do following
+    if((ans>INT_MAX/10) || (ans<INT_MIN/10)){
+        return 0;
+    }
+        digit=(digit*10)+ans;
+        n=n/10;
+    }
+    cout<<digit;
+}
+
